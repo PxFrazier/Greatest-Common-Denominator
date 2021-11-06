bebc1955d6df8ecfe571247a3732bbabac4fff26
Paul Frazier <81392028+PxFrazier@users.noreply.github.com>
2021-11-06 19:00:29 -0400

    Add files via upload

---
+++ GCD.js
@@ -0,0 +1,22 @@
+//finds the greatest common denominator between two integers
+function GCD(num1, num2)
+{
+	let remainder = 1;
+    let result;
+	
+	while(remainder)
+	{
+		remainder = num1 % num2;
+		num1 = num2;
+		num2 = remainder;
+	}
+	
+	num1 == 1 ?
+    result = 'These are coprimes' :
+    result = 'These are not coprimes'
+
+    return result;
+}
+
+console.log(`(2, 4): ${GCD(2, 4)}`); //Outputs 'These are not coprimes'
+console.log(`(3, 10): ${GCD(3, 10)}`); //Outputs 'These are coprimes'
